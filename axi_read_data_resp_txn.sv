e89dc4f13f93301b4b61146211692a4c8511cefe
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-05 15:20:09 +0530

    Update and rename axi_read_data_txn.sv to axi_read_data_resp_txn.sv

--- axi_read_data_resp_txn.sv
+++ axi_read_data_resp_txn.sv
@@ -1,9 +1,9 @@
-class axi_read_data_txn extends uvm_sequence_item;
+class axi_read_data_resp_txn extends uvm_sequence_item;
   rand bit [63:0] rdata_array[]; // Holds data for each beat in a burst
   bit [3:0]       rid;
   rand bit [1:0]  delay_type;    
   rand int        delay_cycles; 
-  `uvm_object_utils(axi_read_data_txn)
+  `uvm_object_utils(axi_read_data_resp_txn)
 
   constraint delay_type_c {
     delay_type inside {[0:2]};
@@ -16,7 +16,7 @@ class axi_read_data_txn extends uvm_sequence_item;
   }
 
   
-  function new(string name = "axi_read_data_txn");
+  function new(string name = "axi_read_data_resp_txn");
     super.new(name);
   endfunction
 endclass
