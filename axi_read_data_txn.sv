260802a998ca1ad7eae6bc1e87a979e7478fc045
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-05 12:31:57 +0530

    Update axi_read_data_txn.sv

--- axi_read_data_txn.sv
+++ axi_read_data_txn.sv
@@ -3,6 +3,17 @@ class axi_read_data_txn extends uvm_sequence_item;
   bit [3:0]       rid;
   `uvm_object_utils(axi_read_data_txn)
 
+  constraint delay_type_c {
+    delay_type inside {[0:2]};
+  }
+
+  constraint delay_cycles_c {
+    if (delay_type == 0) delay_cycles == 0;
+    else if (delay_type == 1) delay_cycles inside {[1:14]};
+    else if (delay_type == 2) delay_cycles == 15;
+  }
+
+  
   function new(string name = "axi_read_data_txn");
     super.new(name);
   endfunction
