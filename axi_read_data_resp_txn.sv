26cad9f047f30c2be2ddcb81c5e8918ee57690a5
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 17:05:37 +0530

    Update axi_read_data_resp_txn.sv

--- axi_read_data_resp_txn.sv
+++ axi_read_data_resp_txn.sv
@@ -1,17 +1,33 @@
 class axi_read_data_resp_txn extends uvm_sequence_item;
-  rand bit [31:0] addr;
-  rand bit [31:0] rdata;
-  rand int delay_type;    
-  rand int data_case;     
-  rand extra_burst_len;
-  //typedef enum {DIRECT, RANDOM, FIXED} rdata_mode_e;
-  //rdata_mode_e rdata_mode;
-
+`uvm_object_utils(axi_read_data_resp_txn)    
+  rand int extra_burst_len;
+  rand int rdata_mode;
+  rand int delay_cycles;
+  
+  bit [63:0] araddr;
+  bit [3:0] arid;
+  int arlen;
+  
   constraint delay_type_c { delay_type inside {0, 1, 2}; }
   constraint rdata_mode_e {rdata_mode inside{0,1,2};}
- // constraint data_case_c  { data_case  inside {0, 1, 2}; }
 
-  `uvm_object_utils(axi_read_data_resp_txn)
+  function void set_dut_arinfo(bit [3:0] id, bit [63:0] addr, int len);
+    arid = id;
+    araddr = addr;
+    arlen = len;
+  endfunction
+
+  function int get_burst_len();
+    return arlen + 1 + extra_burst_len;
+  endfunction
+
+  function bit [63:0] compute_rdata(int i);
+    case (rdata_mode)
+      0: return araddr + i;
+      1: return $urandom();
+      2: return 64'hFFFF_FFFF_FFFF_FFFF;
+    endcase
+  endfunction
 
   function new(string name = "axi_read_data_resp_txn");
     super.new(name);
