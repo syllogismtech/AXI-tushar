e8df9c17ff3070560f7a84141ff633eed2e0695a
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 16:15:15 +0530

    Update axi_read_slave_driver.sv

--- axi_read_slave_driver.sv
+++ axi_read_slave_driver.sv
@@ -15,24 +15,25 @@ task run_phase(uvm_phase phase);
   forever begin
     axi_read_data_resp_txn txn;
     seq_item_port.get_next_item(txn); 
-
+    int burst_len;
+    bit [3:0] arid;
     @(posedge vif.clk);
     if (vif.arvalid && vif.arready) begin
-     // bit [3:0] read_id = vif.arid;
-
+      // bit [3:0] read_id = vif.arid;
+      burst_len=vif.arlen;
+      arid=vif.arid;
       
       repeat (txn.delay_cycles) @(posedge vif.clk);
 
-      for (int i = 0; i <= txn.burst_len; i++) begin
-        vif.rid    <=  vif.arid;
+      for (int i = 0; i <= burst_len; i++) begin
+        vif.rid    <=  arid;
         vif.rvalid <= 1;
-        vif.rlast  <= (i == txn.burst_len);
+        vif.rlast  <= (i == burst_len);
         case (txn.rdata_mode)
-         DIRECT: vif.rdata <= txn.araddr;
+         DIRECT: vif.rdata <= vif.araddr+i;
          RANDOM: vif.rdata <= $urandom();
          FIXED : vif.rdata <= 64'hFFFF_FFFF_FFFF_FFFF;
         endcase
-        vif.rdata  <=txn.rdata;
        // case (txn.arsize)
          // 3'd0: vif.rdata <= txn.rdata_array[i][7:0];
           //3'd1: vif.rdata <= txn.rdata_array[i][15:0];
