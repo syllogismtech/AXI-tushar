8faca2a0a8cf2da60b48348896ab294d7a7c7cef
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 15:26:56 +0530

    Update axi_slave_read_seq.sv

--- axi_slave_read_seq.sv
+++ axi_slave_read_seq.sv
@@ -1,62 +1,34 @@
-class axi_read_seq_delay0 extends uvm_sequence #(axi_read_data_resp_txn);
-  `uvm_object_utils(adder_seq_delay0)
+class axi_read_seq_all_delays extends uvm_sequence #(axi_read_data_resp_txn);
+  `uvm_object_utils(axi_read_seq_all_delays)
 
   axi_read_data_resp_txn txn;
   longint loop_count = 10;
 
-  function new(string name = "axi_read_seq_delay0");
+  function new(string name = "axi_read_seq_all_delays");
     super.new(name);
   endfunction
 
   task body();
     repeat (loop_count) begin
-      txn = adder_sequence_item::type_id::create("txn");
+      txn = axi_read_data_resp_txn::type_id::create("txn");
       start_item(txn);
-      assert(txn.randomize() with { delay_type == 0; });
+      assert(txn.randomize());
       finish_item(txn);
+
+      case (txn.delay_type)
+        0: #(0);
+        1: #( $urandom_range(1, 14) );
+        2: #(15);
+      endcase
+
+      case (txn.data_case)
+        0: txn.rdata = txn.addr;
+        1: txn.rdata = ~txn.addr;
+        2: txn.rdata = 32'hF;
+      endcase
+
+      `uvm_info("SEQ", $sformatf("addr=%h delay_type=%0d data_case=%0d rdata=%h",
+                   txn.addr, txn.delay_type, txn.data_case, txn.rdata), UVM_MEDIUM)
     end
   endtask
 endclass
-
-class axi_read_seq_random_delay extends uvm_sequence #(axi_read_data_resp_txn);
-  `uvm_object_utils(adder_seq_random_delay)
-
-  axi_read_data_resp_txn txn;
-  longint loop_count = 10;
-
-  function new(string name = "axi_read_seq_random_delay");
-    super.new(name);
-  endfunction
-
-  task body();
-    repeat (loop_count) begin
-      txn = adder_sequence_item::type_id::create("txn");
-      start_item(txn);
-      assert(txn.randomize() with { delay_type == 1; });
-      finish_item(txn);
-    end
-  endtask
-endclass
-
-class axi_read_seq_delay15 extends uvm_sequence #(axi_read_data_resp_txn);
-  `uvm_object_utils(adder_seq_delay15)
-
-  axi_read_data_resp_txn txn;
-  longint loop_count = 10;
-
-  function new(string name = "axi_read_seq_delay15");
-    super.new(name);
-  endfunction
-
-  task body();
-    repeat (loop_count) begin
-      txn = adder_sequence_item::type_id::create("txn");
-      start_item(txn);
-      assert(txn.randomize() with { delay_type == 2; });
-      finish_item(txn);
-    end
-  endtask
-endclass
-
-
-
