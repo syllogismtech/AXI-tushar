714cc587e1ace3b0fd74fc4a1dbd13bc12afd3d2
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 15:56:30 +0530

    Update axi_read_data_resp_txn.sv

--- axi_read_data_resp_txn.sv
+++ axi_read_data_resp_txn.sv
@@ -3,6 +3,8 @@ class axi_read_data_resp_txn extends uvm_sequence_item;
   rand bit [31:0] rdata;
   rand int delay_type;    // 0, 1, 2
   rand int data_case;     // 0 = addr, 1 = ~addr, 2 = fixed 'hF
+  typedef enum {DIRECT, RANDOM, FIXED} rdata_mode_e;
+  rdata_mode_e rdata_mode;
 
   constraint delay_type_c { delay_type inside {0, 1, 2}; }
   constraint data_case_c  { data_case  inside {0, 1, 2}; }
