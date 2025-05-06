caf5f31569f6404cde5f462d668f635adc2b3383
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 12:37:07 +0530

    Update axi_read_data_resp_txn.sv

--- axi_read_data_resp_txn.sv
+++ axi_read_data_resp_txn.sv
@@ -2,7 +2,7 @@ class axi_read_data_resp_txn extends uvm_sequence_item;
   rand bit [63:0] rdata_array[]; // Holds data for each beat in a burst
   bit [3:0]       rid;
   rand bit [1:0]  delay_type;    
-  rand int        delay_cycles; 
+  int        delay_cycles; 
   `uvm_object_utils(axi_read_data_resp_txn)
 
   constraint delay_type_c {
