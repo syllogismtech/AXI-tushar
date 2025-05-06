bcafed7874c63e238a150e011c315c0d922e339d
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-06 17:07:33 +0530

    Update axi_read_slave_driver.sv

--- axi_read_slave_driver.sv
+++ axi_read_slave_driver.sv
@@ -15,31 +15,18 @@ task run_phase(uvm_phase phase);
   forever begin
     axi_read_data_resp_txn txn;
     seq_item_port.get_next_item(txn); 
-    int burst_len;
-    bit [3:0] arid;
+
     @(posedge vif.clk);
     if (vif.arvalid && vif.arready) begin
-      burst_len=vif.arlen+1+txn.extra_burst_len;
-      arid=vif.arid;
       
-      repeat (txn.delay_cycles) @(posedge vif.clk);
+      txn.set_dut_arinfo(vif.arid, vif.araddr, vif.arlen);
 
-      for (int i = 0; i <= burst_len; i++) begin
-        vif.rid    <=  arid;
+      repeat (txn.delay_cycles) @(posedge vif.clk);
+      for (int i = 0; i <= txn.get_burst_len(); i++) begin
+        vif.rid    <= txn.arid;
         vif.rvalid <= 1;
-        vif.rlast  <= (i == burst_len);
-        case (txn.rdata_mode)
-         0: vif.rdata <= vif.araddr+i;
-         1: vif.rdata <= $urandom();
-         2 : vif.rdata <= 64'hFFFF_FFFF_FFFF_FFFF;
-        endcase
-       // case (txn.arsize)
-         // 3'd0: vif.rdata <= txn.rdata_array[i][7:0];
-          //3'd1: vif.rdata <= txn.rdata_array[i][15:0];
-          //3'd2: vif.rdata <= txn.rdata_array[i][31:0];
-          //3'd3: vif.rdata <= txn.rdata_array[i][63:0];
-          //default: vif.rdata <= 64'hDEADBEEF_DEADBEEF;
-        //endcase
+        vif.rlast  <= (i == txn.get_burst_len());
+        vif.rdata  <= txn.compute_rdata(i);
 
         @(posedge vif.clk);
         while (!vif.rready) @(posedge vif.clk);
@@ -52,6 +39,7 @@ task run_phase(uvm_phase phase);
     seq_item_port.item_done();  
   end
 endtask
+
 
 
 endclass
