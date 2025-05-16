eea2d36ddf1d3bf342a9264703a9ee0d396a1253
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-16 13:18:52 +0530

    Update interleaved_write_seq.sv

--- interleaved_write_seq.sv
+++ interleaved_write_seq.sv
@@ -12,19 +12,16 @@ class interleaved_write_seq extends uvm_sequence #(uvm_sequence_item);
     axi_transaction m0_write, m1_write;
     axi_transaction m0_read, m1_read;
 
-    // --- Master 0 writes ---
     m0_write = axi_transaction::type_id::create("m0_write");
     m0_write.addr = 32'h0002_0700;
     m0_write.data = 32'hAAAA0001;
     m0_write.trans_type = WRITE;
 
-    // --- Master 1 writes ---
     m1_write = axi_transaction::type_id::create("m1_write");
     m1_write.addr = 32'h0002_0704;
     m1_write.data = 32'hBBBB0002;
     m1_write.trans_type = WRITE;
 
-    // Start both writes in parallel
     fork
       begin
         start_item_on(m0_write, m0_seqr);
@@ -36,7 +33,6 @@ class interleaved_write_seq extends uvm_sequence #(uvm_sequence_item);
       end
     join
 
-    // --- Master 0 reads ---
     m0_read = axi_transaction::type_id::create("m0_read");
     m0_read.addr = 32'h0002_0700;
     m0_read.trans_type = READ;
@@ -46,7 +42,6 @@ class interleaved_write_seq extends uvm_sequence #(uvm_sequence_item);
     m1_read.addr = 32'h0002_0704;
     m1_read.trans_type = READ;
 
-    // Start both reads in parallel
     fork
       begin
         start_item_on(m0_read, m0_seqr);
