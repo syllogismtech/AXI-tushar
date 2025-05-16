3064bd7433f163009883fa48f871feccb46541cf
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-16 13:13:03 +0530

    Update and rename cross_master_rw_seq.sv to cross_master_rw_rd_seq.sv

--- cross_master_rw_rd_seq.sv
+++ cross_master_rw_rd_seq.sv
@@ -1,17 +1,16 @@
-class cross_master_rw_seq extends uvm_sequence #(uvm_sequence_item);
-  `uvm_object_utils(cross_master_rw_seq)
+class cross_master_rw_rd_seq extends uvm_sequence #(uvm_sequence_item);
+  `uvm_object_utils(cross_master_rw_rd_seq)
 
   axi_master_sequencer m0_seqr;
   axi_master_sequencer m1_seqr;
 
-  function new(string name = "cross_master_rw_seq");
+  function new(string name = "cross_master_rw_rd_seq");
     super.new(name);
   endfunction
 
   task body();
     axi_transaction write_tr, read_tr;
 
-    // 1. Master 1 writes to Slave 1
     write_tr = axi_transaction::type_id::create("write_tr");
     write_tr.addr = 32'h0001_0100;
     write_tr.data = 32'hCAFEBABE;
@@ -21,7 +20,6 @@ class cross_master_rw_seq extends uvm_sequence #(uvm_sequence_item);
     start_item_on(write_tr, m1_seqr);
     finish_item_on(write_tr, m1_seqr);
 
-    // 2. Master 0 reads from same address
     read_tr = axi_transaction::type_id::create("read_tr");
     read_tr.addr = 32'h0001_0100;
     read_tr.trans_type = READ;
@@ -30,7 +28,6 @@ class cross_master_rw_seq extends uvm_sequence #(uvm_sequence_item);
     start_item_on(read_tr, m0_seqr);
     finish_item_on(read_tr, m0_seqr);
 
-    // Optional: print data
     `uvm_info(get_type_name(), $sformatf("Master 0 read data: 0x%08X", read_tr.data), UVM_LOW)
   endtask
 endclass
